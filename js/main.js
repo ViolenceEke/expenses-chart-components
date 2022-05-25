bcb2fa01ff491c918757a017d11a4976e1138876
Ilya <violenceeke@gmailcom>
2022-05-25 19:55:59 +1000

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+Chart.defaults.global.legend.display = false;
+
+const ctx = document.getElementById('myChart').getContext('2d')
+
+const data = database;
+
+const barChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: data.map(index => {
+            return index.day
+        }),
+        datasets: [
+            {
+                data: data.map(index => {
+                    return index.amount
+                }),
+                backgroundColor: [
+                    'hsl(10, 79%, 65%)',
+                    'hsl(10, 79%, 65%)',
+                    'hsl(186, 34%, 60%)',
+                    'hsl(10, 79%, 65%)',
+                    'hsl(10, 79%, 65%)',
+                    'hsl(10, 79%, 65%)',
+                    'hsl(10, 79%, 65%)',
+                    'hsl(10, 79%, 65%)'
+                ],
+            }
+        ]
+    },
+    options: {
+        scales: {
+            xAxes: [
+                {
+                    gridLines: {
+                        display: false,
+                    },
+                }
+            ],
+            yAxes: [
+                {
+                    display: false,
+                    gridLines: {
+                        display: false,
+                    }
+                }
+            ],
+        },
+        borderSkipped: false,
+        cornerRadius: 7,
+    }
+});
